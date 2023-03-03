6408a5960e22ec98a1544491b7a119bdf4b39e0d
Thgear27 <vegasfernando2003@gmail.com>
2023-03-03 16:44:57 -0500

    Cube faces fixed

--- src/CubeRenderer.cpp
+++ src/CubeRenderer.cpp
@@ -3,65 +3,63 @@
 CubeRenderer::CubeRenderer(ShaderProgram& programToBeUsed)
     : m_shaderProgram(programToBeUsed) {
     verticesData = {
-        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, //
-        0.5f,  -0.5f, -0.5f, 1.0f, 0.0f, //
-        0.5f,  0.5f,  -0.5f, 1.0f, 1.0f, //
-        0.5f,  0.5f,  -0.5f, 1.0f, 1.0f, //
-        -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f, //
-        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, //
+        // BACK FACE
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, //
+         0.5f, -0.5f, -0.5f, 0.0f, 1.0f, //
+         0.5f,  0.5f, -0.5f, 0.0f, 0.0f, //
+         0.5f,  0.5f, -0.5f, 0.0f, 0.0f, //
+        -0.5f,  0.5f, -0.5f, 1.0f, 0.0f, //
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, //
 
-        -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, //
-        0.5f,  -0.5f, 0.5f,  1.0f, 0.0f, //
-        0.5f,  0.5f,  0.5f,  1.0f, 1.0f, //
-        0.5f,  0.5f,  0.5f,  1.0f, 1.0f, //
-        -0.5f, 0.5f,  0.5f,  0.0f, 1.0f, //
-        -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, //
+        // FRONT FACE
+         0.5f, -0.5f,  0.5f, 1.0f, 1.0f, //
+        -0.5f, -0.5f,  0.5f, 0.0f, 1.0f, //
+        -0.5f,  0.5f,  0.5f, 0.0f, 0.0f, //
+        -0.5f,  0.5f,  0.5f, 0.0f, 0.0f, //
+         0.5f,  0.5f,  0.5f, 1.0f, 0.0f, //
+         0.5f, -0.5f,  0.5f, 1.0f, 1.0f, //
 
+        // LEFT FACE
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, //
+        -0.5f, 0.5f,  -0.5f, 0.0f, 0.0f, //
         -0.5f, 0.5f,  0.5f,  1.0f, 0.0f, //
-        -0.5f, 0.5f,  -0.5f, 1.0f, 1.0f, //
-        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, //
-        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, //
-        -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, //
-        -0.5f, 0.5f,  0.5f,  1.0f, 0.0f, //
-
-        0.5f,  0.5f,  0.5f,  1.0f, 0.0f, //
-        0.5f,  0.5f,  -0.5f, 1.0f, 1.0f, //
-        0.5f,  -0.5f, -0.5f, 0.0f, 1.0f, //
-        0.5f,  -0.5f, -0.5f, 0.0f, 1.0f, //
-        0.5f,  -0.5f, 0.5f,  0.0f, 0.0f, //
-        0.5f,  0.5f,  0.5f,  1.0f, 0.0f, //
-
-        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, //
-        0.5f,  -0.5f, -0.5f, 1.0f, 1.0f, //
-        0.5f,  -0.5f, 0.5f,  1.0f, 0.0f, //
-        0.5f,  -0.5f, 0.5f,  1.0f, 0.0f, //
-        -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, //
+        -0.5f,  0.5f,  0.5f, 1.0f, 0.0f, //
+        -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, //
         -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, //
 
-        -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f, //
-        0.5f,  0.5f,  -0.5f, 1.0f, 1.0f, //
-        0.5f,  0.5f,  0.5f,  1.0f, 0.0f, //
-        0.5f,  0.5f,  0.5f,  1.0f, 0.0f, //
-        -0.5f, 0.5f,  0.5f,  0.0f, 0.0f, //
-        -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f  //
+        // RIGHT FACE
+        0.5f,  0.5f, -0.5f, 1.0f, 0.0f, //
+        0.5f, -0.5f, -0.5f, 1.0f, 1.0f, //
+        0.5f, -0.5f,  0.5f, 0.0f, 1.0f, //
+        0.5f, -0.5f,  0.5f, 0.0f, 1.0f, //
+        0.5f,  0.5f,  0.5f, 0.0f, 0.0f, //
+        0.5f,  0.5f, -0.5f, 1.0f, 0.0f, //
+
+        // DOWN FACE
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, //
+         0.5f, -0.5f, -0.5f, 1.0f, 1.0f, //
+         0.5f, -0.5f,  0.5f, 1.0f, 0.0f, //
+         0.5f, -0.5f,  0.5f, 1.0f, 0.0f, //
+        -0.5f, -0.5f,  0.5f, 0.0f, 0.0f, //
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, //
+
+        // UP FACE
+        -0.5f, 0.5f,  0.5f, 0.0f, 1.0f, //
+         0.5f, 0.5f,  0.5f, 1.0f, 1.0f, //
+         0.5f, 0.5f, -0.5f, 1.0f, 0.0f, //
+         0.5f, 0.5f, -0.5f, 1.0f, 0.0f, //
+        -0.5f, 0.5f, -0.5f, 0.0f, 0.0f, //
+        -0.5f, 0.5f,  0.5f, 0.0f, 1.0f  //
     };
 
-    uint indices[] = {
-        0, 1, 3, // first triangle
-        1, 2, 3  // second triangle
-    };
-
-    m_cubeTexture.loadImageData("resources/img/textures.png", 0, GL_RGBA, GL_RGBA);
+    m_cubeTexture.loadImageData("resources/img/sdlLogo.png", 0, GL_RGBA, GL_RGBA);
     m_cubeTexture.setTextureUnit(0);
 
     m_vbo.loadData(sizeof(verticesData), verticesData.begin(), GL_STATIC_DRAW);
     m_vbo.push_VertexAttribLayout(GL_FLOAT, GL_FALSE, 3);
     m_vbo.push_VertexAttribLayout(GL_FLOAT, GL_FALSE, 2);
 
-    m_ebo.loadData(sizeof(indices), indices, GL_STATIC_DRAW);
-
     m_vao.addVertexBuffer(&m_vbo);
-    m_vao.addElementBuffer(&m_ebo);
     m_vao.linkBuffers();
 }
 
--- src/CubeRenderer.hpp
+++ src/CubeRenderer.hpp
@@ -1,14 +1,14 @@
 #ifndef __CUBERENDERER_H__
 #define __CUBERENDERER_H__
 
+#include "OpenGL/Buffers/ElementBuffer.hpp"
+#include "OpenGL/Buffers/VertexArray.hpp"
+#include "OpenGL/Buffers/VertexBuffer.hpp"
+#include "OpenGL/GLerror.hpp"
+#include "OpenGL/ShaderProgram.hpp"
+#include "OpenGL/Texture2D.hpp"
 #include <array>
 #include <glad/glad.h>
-#include "OpenGL/Buffers/VertexBuffer.hpp"
-#include "OpenGL/Buffers/VertexArray.hpp"
-#include "OpenGL/Buffers/ElementBuffer.hpp"
-#include "OpenGL/Texture2D.hpp"
-#include "OpenGL/ShaderProgram.hpp"
-#include "OpenGL/GLerror.hpp"
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -20,7 +20,6 @@ private:
     // std::array<GLuint, 36> indicesData;
     VertexBuffer m_vbo;
     VertexArray m_vao;
-    ElementBuffer m_ebo;
 
     Texture2D m_cubeTexture; // This texture have all the cubes textures
     ShaderProgram& m_shaderProgram;
@@ -31,6 +30,5 @@ public:
 
     void render(const glm::vec3& position);
 };
-
 
 #endif // __CUBERENDERER_H__
